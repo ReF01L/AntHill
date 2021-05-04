7ceb7ecbb8189db42d595afd5878d4771e6ef257
Dmitry <salushkin1998@bk.ru>
2021-05-04 23:31:35 +1000

    Update user data and add edit forms\page

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,6 @@ app_name = 'account'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='user_login'),
-    path('logout/', views.user_logout, name='logout')
+    path('logout/', views.user_logout, name='logout'),
+    path('edit/', views.edit, name='edit')
 ]
