23ad19f687e17dcb6c0581eac4d25784b4654b43
Dmitry <salushkin1998@bk.ru>
2021-05-10 15:33:46 +1000

    Завершение авторизации\Изменения профиля

--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
@@ -15,13 +16,16 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
-        return render(request, 'account/edit.html', {
-            'user_form': user_form,
-            'profile_form': profile_form
-        })
+    return render(request, 'account/edit.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
 
 
 def user_logout(request):
@@ -54,7 +58,7 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
-            profile = Profile.objects.get(user=new_user)
+            Profile.objects.create(user=new_user)
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
