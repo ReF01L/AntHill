436b7678c919c28b656908e104ca873031ac26be
MagHErmit <vladkakurin007@gmail.com>
2021-06-08 20:59:20 +1000

    add partition of project template

--- project/views.py
+++ project/views.py
@@ -1,15 +1,21 @@
-from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_GET
-from pytz import unicode
 
 from AntHill import settings
 from account.models import Profile
 from project.models import Project
 
 
+@login_required
 def projects(request):
-    return render(request, 'project/projects.html')
+    user = Profile.objects.get(user=request.user)
+    _projects = user.project_set.all()
+    if _projects:
+        return render(request, 'project/project_exists.html', {
+            'projects': user.project_set.all()
+        })
+    return render(request, 'project/project_not_exists.html')
 
 
 def command(request, project):
@@ -27,3 +33,11 @@ def recent_project(request):
     var = Profile.objects.get(user=request.user)
     project_set = var.project_set.all()
     return render(request, 'project/projects.html', {'project_names': [x.name for x in project_set]})
+
+
+def create_project(request):
+    return redirect('project:projects')
+
+
+def join_project(request):
+    return redirect('project:projects')
