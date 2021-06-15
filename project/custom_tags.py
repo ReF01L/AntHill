f126406e0b4f4a7ffdb86a94f6c2a4a396820886
ReF01L <salushkin1998@bk.ru>
2021-06-16 01:09:07 +1000

    Create Sprint page, Edit sprint Page, fix board

--- project/custom_tags.py
+++ project/custom_tags.py
@@ -4,3 +4,8 @@ from django.template.defaulttags import register
 @register.filter
 def inverse(flag):
     return not flag
+
+
+@register.filter
+def cut_title(title: str) -> str:
+    return f'{title[:33]}...' if len(title) > 36 else title
